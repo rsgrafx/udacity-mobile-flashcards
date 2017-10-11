3e312d78c3a91d32d2f1f4d9229fdb4726d7c622
Orion Engleton <rsgrafx@gmail.com>
2017-10-10 17:22:17 -0700

    Feature Just added boilerplate for AddCard
    Will follow up and make Form functional in next commit - base UI implemented

--- components/AddCard/index.js
+++ components/AddCard/index.js
@@ -1,14 +1,67 @@
 import React, {Component} from 'react'
+
 import {
   View,
-  Text
+  Text,
+  TextInput,
+  TouchableOpacity
 } from 'react-native'
+import {NavigationActions}  from 'react-navigation'
 
-const AddCard = (props) => {
-  return(
-  <View style={{flex: 1, justifyContent: 'center'}}>
-    <Text>Add Card Screen</Text>
-  </View>)
+import styles from './style'
+
+class AddCard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: null,
+      answerA: null,
+      answerB: null
+    }
+    console.log(this.props)
+    this.submitForm.bind(this)
+  }
+
+  submitForm(nav) {
+    nav.goBack()
+  }
+
+  render() {
+    return(
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 20}}>Add Card to: </Text>
+        <Text style={{fontSize: 20}}>{this.props.navigation.state.params.name} Deck</Text>
+      </View>
+      <View style={{flex: 1, padding: 10}}>
+        <Text>Question Detail:</Text>
+        <TextInput
+          style={{height: 40, borderBottomColor: 'grey', borderWidth: 1}}
+          value={this.state.AnswerA} />
+      </View>
+      <View style={{flex: 2, marginTop: 10, padding: 10}}>
+        <Text>Correct Answer:</Text>
+        <TextInput
+          style={{height: 40, borderBottomColor: 'grey', borderWidth: 1}}
+          value={this.state.AnswerA}
+          />
+        <Text>Incorrect Answer: Use hint to throw off.</Text>
+        <TextInput
+          style={{height: 40, borderBottomColor: 'grey', borderWidth: 1}}
+          value={this.state.answerB}
+          />
+      </View>
+      <View style={{flex: 1, padding: 10}}>
+        <TouchableOpacity
+          onPress={() => {this.submitForm(this.props.navigation) }}
+          style={styles.btn}>
+            <Text style={styles.btnText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    )
+  }
 }
 
 export default AddCard
---
+++ components/AddCard/style.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  btn: {
+    alignItems: 'center',
+    alignContent: 'center',
+    backgroundColor: '#34495e',
+    borderRadius: 3,
+    padding: 20,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15
+  }
+})
+
+export default styles
