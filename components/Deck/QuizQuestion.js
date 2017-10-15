8539488f0ff5891efe37fae4b63e4cc2a61bc2ea
Orion Engleton <rsgrafx@gmail.com>
2017-10-14 22:05:11 -0700

    fix: Linter cleanup - Connected component to state
    In order to store scores are they are done I had to create new actions to be dispatched. integrated state attributes with component

--- components/Deck/QuizQuestion.js
+++ components/Deck/QuizQuestion.js
@@ -1,50 +1,76 @@
-import React, {Component} from 'react'
-import {View, Text, TouchableHighlight, StyleSheet} from 'react-native'
-import {NavigationActions}  from 'react-navigation'
-import FlipCard from 'react-native-flip-card'
-import {updateQuizScore} from '../../stores/actions'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import FlipCard from 'react-native-flip-card';
+import { connect } from 'react-redux';
 
-import styles from  './styles'
+import { updateQuizScore } from '../../stores/actions';
+import styles from './styles';
 
 class QuizQuestion extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       flip: false,
       question_idx: 0,
       answer: 'wrong',
       complete: false
+    };
+    this.changeQuestion.bind(this);
+  }
+
+  componentDidUpdate() {
+    const key = this.props.navigation.state.params.key;
+    const count = this.props.questions.length;
+    const idx = (this.props.quizScore.qIndex + 1);
+    if (count <= idx) {
+      this.props.storeScore({ ...this.props.quizScore, qCount: count, quiz: key });
     }
-    this.changeQuestion.bind(this)
   }
 
   changeQuestion(idx) {
-      const {questions} = this.props
+      const { questions } = this.props;
       this.setState((previousState) => {
-        if (previousState.question_idx >= (questions.length-1)) {
+        if (previousState.question_idx >= (questions.length - 1)) {
           return {
-            question_idx: questions.length-1,
+            question_idx: questions.length - 1,
             flip: !this.state.flip,
             answer: 'new',
-            complete: true}
+            complete: true
+          };
         } else {
-          return {question_idx: previousState.question_idx+1,
+          return { question_idx: previousState.question_idx + 1,
             flip: !this.state.flip,
-            answer: 'new'}
+            answer: 'new'
+          };
         }
       }
-    )
+    );
   }
 
   flipForAnswer(idx, val) {
-    const {questions} = this.props
+    const { questions } = this.props;
     const complete = (this.state.question_idx === questions.length-1)
+
+    const payload = this.scorePayload(
+      questions.length,
+      this.state.question_idx,
+      val);
+
+    this.props.answerQuestion(payload);
     if (val === 'correct') {
-      this.props.updateScore()
-      this.setState({flip: !this.state.flip, answer: val, complete})
+      this.setState({ flip: !this.state.flip, answer: val, complete });
     } else {
-      this.setState({flip: !this.state.flip, complete})
+      this.setState({ flip: !this.state.flip, complete });
+    }
+  }
+
+  scorePayload(qCount, qIndex, answer) {
+    const value = (answer === 'correct') ? 1 : 0;
+    return {
+      qCount,
+      qIndex,
+      value
     }
   }
 
@@ -54,100 +80,106 @@ class QuizQuestion extends Component {
       actions: [
         NavigationActions.navigate({ routeName: 'Main'})
       ]
-    })
+    });
     this.props.navigation.dispatch(resetAction)
   }
 
   backToDecks() {
-    return(
+    return (
       <TouchableHighlight
         style={styles.button}
         onPress={() => {this.returnToDecks()} }>
         <Text>Try Another Quiz</Text>
       </TouchableHighlight>
-    )
+    );
   }
 
   nextQuestion() {
-    return(
+    return (
     <TouchableHighlight
       style={styles.button}
-      onPress={() => {this.changeQuestion(this.state.question_idx)} }>
+      onPress={() => { this.changeQuestion(this.state.question_idx); }}
+    >
       <Text>Next Question</Text>
-    </TouchableHighlight>)
+    </TouchableHighlight>
+    );
   }
 
   answerQuestion(answerStatus, hint, additionalStyles={}) {
-    const correct = answerStatus ? 'correct' : 'wrong'
-    return(
+    const correct = answerStatus ? 'correct' : 'wrong';
+    return (
       <TouchableHighlight
         key={hint}
         style={[styles.button, additionalStyles]}
         onPress={() => {this.flipForAnswer(this.state.question_idx, correct)} }>
         <Text style={{fontSize: 18}}>{hint}</Text>
       </TouchableHighlight>
-    )
+    );
   }
 
   remainingQuestions(qlength, idx) {
     const count = qlength - idx
-    return(
-      <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'flex-end' }}>
         {count === 1 ? <Text>Final Question</Text> : <Text>{count} Remaining</Text> }
       </View>
-    )
+    );
   }
 
   retryQuiz() {
-    return(
+    return (
       <TouchableHighlight
         style={[styles.button, {backgroundColor: 'red'}]}
         onPress={() => {
           this.props.resetQuiz()
+          this.props.retryQuizReset(this.props.navigation.state.params.key)
           this.props.navigation.goBack()}
-          }>
+          }
+        >
         <Text>Retry Quiz</Text>
       </TouchableHighlight>
-    )
+    );
   }
 
   finalCard() {
-    return(<View>
+    return (
+     <View>
       <Text>Quiz Completed</Text>
       <Text>{this.props.quizScore.score} / {this.props.questions.length }</Text>
       {this.retryQuiz()}
       {this.backToDecks()}
-    </View>)
+    </View>
+    );
   }
 
   render() {
-    const {questions} = this.props
-    const question = questions[this.state.question_idx]
-    return(
-        <View style={{flex: 1, alignSelf: 'stretch', backgroundColor: '#B3E5FC'}}>
+    const { questions } = this.props;
+    const question = questions[this.state.question_idx];
+    return (
+        <View style={{ flex: 1, alignSelf: 'stretch', backgroundColor: '#B3E5FC' }}>
           <FlipCard
             flip={this.state.flip}
-            alignHeight={true}
+            alignHeight
             perspective={1000}
             clickable={false}
-            style={{flex: 1, borderRadius: 5, borderColor: '#2980b9', padding: 10}}>
-            <View style={{flex: 1}}>
-
-              <View style={{flex: 2, alignItems: 'center', justifyContent: 'center', padding: 5}}>
-                <Text style={{fontSize: 30}}>{question.title}</Text>
+            style={{ flex: 1, borderRadius: 5, borderColor: '#2980b9', padding: 10 }}
+          >
+            <View style={{ flex: 1 }}>
+              <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center', padding: 5 }}>
+                <Text style={{ fontSize: 30 }}>{question.title}</Text>
               </View>
 
-              <View style={{flex: 1}}>
+              <View style={{ flex: 1 }}>
                 {question.answers.map((answer) =>
                   this.answerQuestion(answer.correct, answer.hint))
                 }
               </View>
               {this.remainingQuestions(questions.length, this.state.question_idx)}
             </View>
-            <View style={{flex: 1}}>
+            <View style={{ flex: 1 }}>
               {(this.state.answer === 'correct')
                 ? <View style={styles.flex1centered}>
-                    <Text style={{fontSize: 30}}>Correct!</Text>
+                    <Text style={{ fontSize: 30 }}>Correct!</Text>
                     { this.state.complete
                       ? this.finalCard()
                       : this.nextQuestion()
@@ -165,20 +197,31 @@ class QuizQuestion extends Component {
             </View>
           </FlipCard>
         </View>
-    )
+    );
   }
 }
 
 export default connect((state) => (
-  {quizScore: state.currentQuiz}
+  {
+    quizScore: state.currentQuiz
+  }
   ), (dispatch) => ({
-  updateScore() {
-    dispatch(updateQuizScore())
+  updateScore(qIndex) {
+    dispatch(updateQuizScore(qIndex));
+  },
+  answerQuestion(payload) {
+    dispatch({ type: 'ANSWER_QUESTION', payload });
   },
   score() {
-    dispatch({type: 'QUIZ_SCORE'})
+    dispatch({ type: 'QUIZ_SCORE' });
   },
   resetQuiz() {
-    dispatch({type: 'RESET_QUIZ'})
+    dispatch({ type: 'RESET_QUIZ' });
+  },
+  storeScore(payload) {
+    dispatch({ type: 'STORE_SCORE', payload });
+  },
+  retryQuizReset(quizKey) {
+    dispatch({ type: 'RETRY_QUIZ', payload: { quizKey } });
   }
-}))(QuizQuestion)
+}))(QuizQuestion);
